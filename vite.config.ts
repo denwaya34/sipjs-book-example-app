6a1712575bbf56d3884b697a43733c54a9bdaa00
denwaya34 <denwaya34@gmail.com>
2025-05-29 08:41:41 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 import tsconfigPaths from 'vite-tsconfig-paths';
 import * as path from "node:path";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,6 +15,13 @@ export default defineConfig({
       projects: [
         './tsconfig.json',
       ],
+    }),
+    checker({
+      typescript: true,
+      eslint: {
+        useFlatConfig: true,
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
     }),
     react(),
     tailwindcss(),
