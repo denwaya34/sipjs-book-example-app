310160fb0b3090cb587a1afda3aeb9f777b5e7a9
denwaya34 <denwaya34@gmail.com>
2025-05-30 01:07:15 +0900

    Fix dependency resolution and ESLint configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,22 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-// eslint-disable-next-line import/no-named-as-default
-import checker from 'vite-plugin-checker';
+import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/sipjs-book-example-app/',
-  server: {
-    open: true,
-  },
   plugins: [
+    react(),
     tsconfigPaths(),
     checker({
       typescript: true,
       eslint: {
-        useFlatConfig: true,
         lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
       },
     }),
-    react(),
-    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 });
