d26064b6a7e9b29e2752ee0977b6030265de8fc3
denwaya34 <denwaya34@gmail.com>
2025-05-29 09:05:47 +0900

    init

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,39 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import unusedImports from "eslint-plugin-unused-imports";
-import reactX from 'eslint-plugin-react-x'
-import reactDom from 'eslint-plugin-react-dom'
-import stylistic from '@stylistic/eslint-plugin'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
+import reactX from 'eslint-plugin-react-x';
+import reactDom from 'eslint-plugin-react-dom';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default tseslint.config(
   {
-    ignores: ['dist']
+    ignores: ['dist'],
   },
   {
+    files: ['**/*.{ts,tsx,js,jsx}'],
     extends: [
       js.configs.recommended,
       stylistic.configs.customize({
         semi: true,
       }),
+    ],
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
       ...tseslint.configs.strictTypeChecked,
       ...tseslint.configs.stylisticTypeChecked,
     ],
-    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
         project: [
           './tsconfig.node.json',
-          './tsconfig.app.json'
+          './tsconfig.app.json',
         ],
         tsconfigRootDir: import.meta.dirname,
       },
@@ -38,18 +43,18 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
       'react-x': reactX,
       'react-dom': reactDom,
-      "unused-imports": unusedImports,
+      'unused-imports': unusedImports,
     },
     rules: {
-     "@typescript-eslint/no-unused-vars": "off",
-      "unused-imports/no-unused-imports": "error",
-      "unused-imports/no-unused-vars": [
-        "warn",
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
         {
-          "vars": "all",
-          "varsIgnorePattern": "^_",
-          "args": "after-used",
-          "argsIgnorePattern": "^_",
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
         },
       ],
       ...reactHooks.configs.recommended.rules,
@@ -61,4 +66,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+);
