237d39791640003fa50e3f4b2478e6f53ad782e4
denwaya34 <denwaya34@gmail.com>
2025-05-29 21:03:56 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sipjs-book-example-app/',
   server: {
     open: true,
   },
@@ -21,4 +22,4 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-})
+});
