1de2639c3e2bf6302e1d010148a6bf8b390816dd
denwaya34 <denwaya34@gmail.com>
2025-05-29 09:12:22 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-
-import {Button} from "@/components/ui/button.tsx";
+import { Button } from '@/components/ui/button.tsx';
 
 function App() {
-
   return (
     <>
       <h1>Hello</h1>
       <Button>Click me</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
